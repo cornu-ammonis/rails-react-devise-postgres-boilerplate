3e40bbcfd86eac1015ed79374a7358147c163631
Andrew Jones <andrew@bidops.com>
2019-12-28 14:04:17 -0500

    initial implementation of ParseApiResponse

--- client/src/api/ApiUtils.js
+++ client/src/api/ApiUtils.js
@@ -1,5 +1,24 @@
 import axios from 'axios';
 
+export const ParseApiResponse = (success, response, options) => {
+  const result = { success };
+
+  if (!success) {
+    // TODO: figure out if theres another path to handle here.
+    result.errors = response.response.data.errors;
+    return result;
+  }
+
+  const responseKeys = options.responseDataKeys || {};
+
+  Object.keys(responseKeys).forEach(objKey => {
+    const railsKey = responseKeys[objKey];
+    result[objKey] = response.data[railsKey];
+  });
+
+  return result;
+}
+
 export const Call = (method, url, data, options = {}, parseCallback) => {
   return new Promise(resolve => {
     axios({
