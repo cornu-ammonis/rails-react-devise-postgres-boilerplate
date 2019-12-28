3404bc8496787c6347647914dabdc920f68ddc0a
Andrew Jones <andrew@bidops.com>
2019-12-28 14:18:05 -0500

    get ParseApiResponse to handle scenario where rails does not manually add a key

--- client/src/api/ApiUtils.js
+++ client/src/api/ApiUtils.js
@@ -13,13 +13,17 @@ export const ParseApiResponse = (success, response, options) => {
 
   Object.keys(responseKeys).forEach(objKey => {
     const railsKey = responseKeys[objKey];
-    result[objKey] = response.data[railsKey];
+
+    if (response.data[railsKey])
+      result[objKey] = response.data[railsKey];
+    else
+      result[objKey] = response.data;
   });
 
   return result;
 }
 
-export const Call = (method, url, data, options = {}, parseCallback) => {
+export const Call = (method, url, data, options = {}, parseCallback = ParseApiResponse) => {
   return new Promise(resolve => {
     axios({
       method,
