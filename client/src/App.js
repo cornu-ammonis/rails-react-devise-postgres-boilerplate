d92c9b36e12dcd6e797a4def8ba38e3f67c45d9d
Andrew Jones <andrew@bidops.com>
2020-01-02 17:59:42 -0500

    try moving css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {
-  HashRouter as Router, 
+  HashRouter as Router,
   Route
 } from 'react-router-dom';
 import PostList from './components/PostList';
 import NewPost from './components/NewPost';
-import './App.css';
 
 function App() {
   return (
